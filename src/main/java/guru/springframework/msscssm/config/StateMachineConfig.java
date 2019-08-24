96706eaf7722c21dd54f4ce9a23ffd655351d3c7
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-08-24 06:42:13 -0400

    Adding state machine config for states

---
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -0,0 +1,30 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+/**
+ * Created by Donald F. Coffin on 08/24/2019 at 06:32
+ */
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}
--- src/main/java/guru/springframework/msscssm/domain/PaymentState.java
+++ src/main/java/guru/springframework/msscssm/domain/PaymentState.java
@@ -4,5 +4,5 @@ package guru.springframework.msscssm.domain;
  * Created by Donald F. Coffin on 08/24/2019 at 06:17
  */
 public enum PaymentState {
-    NEW, PRE_AUTH, pRE_AUTH_ERROR, AUTH, AUTH_ERROR
+    NEW, PRE_AUTH, PRE_AUTH_ERROR, AUTH, AUTH_ERROR
 }
