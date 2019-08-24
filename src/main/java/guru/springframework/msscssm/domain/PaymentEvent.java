7ead22e0d0623421bcdba44c323207f1d730637b
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-08-24 06:20:34 -0400

    Adding enums for states and events

---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscssm.domain;
+
+/**
+ * Created by Donald F. Coffin on 08/24/2019 at 06:18
+ */
+public enum PaymentEvent {
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
+}
---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentState.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscssm.domain;
+
+/**
+ * Created by Donald F. Coffin on 08/24/2019 at 06:17
+ */
+public enum PaymentState {
+    NEW, PRE_AUTH, pRE_AUTH_ERROR, AUTH, AUTH_ERROR
+}
