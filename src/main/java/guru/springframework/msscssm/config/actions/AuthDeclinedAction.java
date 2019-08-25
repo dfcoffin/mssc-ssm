29cb11f3e1e389bda32024a6c5472df6ce9fef07
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-08-25 17:53:28 -0400

    Adding notification actions

---
+++ src/main/java/guru/springframework/msscssm/config/actions/AuthDeclinedAction.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscssm.config.actions;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Donald F. Coffin on 08/25/2019 at 17:42
+ */
+@Component
+public class AuthDeclinedAction implements Action<PaymentState, PaymentEvent> {
+    @Override
+    public void execute(StateContext<PaymentState, PaymentEvent> context) {
+        System.out.println("Sending Notification of Auth Declined");
+    }
+}
